6c9ba863e1624b17deda19a774f93fa73b2e0baa
w0ram <55700217+w0ram@users.noreply.github.com>
2024-03-27 12:17:43 +0000

    @cors

--- MicroserviceUser/src/main/java/com/helloIftekhar/springJwt/controller/AuthenticationController.java
+++ MicroserviceUser/src/main/java/com/helloIftekhar/springJwt/controller/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 public class AuthenticationController {
 
