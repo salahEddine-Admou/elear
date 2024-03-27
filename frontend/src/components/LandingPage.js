805ed568297796ea217b4b55ca569f6b6928dda1
w0ram <55700217+w0ram@users.noreply.github.com>
2024-03-27 12:32:02 +0000

    FRONTEND

---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+function LandingPage() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+
+
+export default LandingPage;
