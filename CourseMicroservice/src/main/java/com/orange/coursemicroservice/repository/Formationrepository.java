f1cead5b35bae0b965efb9621c56effa01ffacac
Mina <128073566+Minaaa2@users.noreply.github.com>
2024-04-18 10:11:01 +0100

    Delete CourseMicroservice directory

--- CourseMicroservice/src/main/java/com/orange/coursemicroservice/repository/Formationrepository.java
+++
@@ -1,8 +0,0 @@
-package com.orange.coursemicroservice.repository;
-
-import com.orange.coursemicroservice.model.Formation;
-import org.springframework.data.mongodb.core.MongoAdminOperations;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Formationrepository extends MongoRepository<Formation, String> {
-}
